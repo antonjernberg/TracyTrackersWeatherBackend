831e5c12bf10de8c82c510694a64fb6441b7454d
anton <anton@antons-Mac.local>
2017-05-28 01:29:50 +0200

    Version 1.0 Everything working except pressure compensation formula

--- bluetooth sensor/AutomatCommunication.swift
+++ bluetooth sensor/AutomatCommunication.swift
@@ -12,44 +12,26 @@ import NeueLabsAutomat
 
 class AutomatCommunication {
     // A timer used to continuously update pressure every 30 min
-    var timer: Timer?
-    var timerForCountingAverage: Timer?
-    var arrayForCountingAverage: [Double]?
+    private var timer: Timer?
+    private var timerForCountingAverage: Timer?
+    private var arrayForCountingAverage: [Double]?
+    private var compensations: Compensations = Compensations()
     
     // An object representing a connection manager used to connect with the Automat board
-    let automatConnectionManager = NLAConnectionManager()
+    private let automatConnectionManager = NLAConnectionManager()
     
     // An object representing the Automat baseboard. Provides access to the functionality of the Automat board
-    var automatBaseboard: NLABaseBoard?
-    var automatDevice : NLAAutomatDevice?
-    var averageChangeOfPressurePerHour : Double? = 0.0
+    private var automatBaseboard: NLABaseBoard?
+    private var automatDevice : NLAAutomatDevice?
+    private var averageChangeOfPressurePerHour : Double? = 0.0
     
     // A local array for storing DataPoints
-    var pressureStorage: [DataPoint]!
+    private var pressureStorage: [DataPoint]!
     
     // Pointers to UInt8 arrays containing commands for writing to the registers of a BME280 sensor
-    let pointerToStartSensorCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
-    let pointerToStartFilterCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
-    let pointerToStartResetCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
-    
-    
-    // Variables for storing Calibration Parameters from the BME280 sensor
-    // Do not access these before the sensors have been started
-    var T1: UInt16!
-    var T2: Int16!
-    var T3: Int16!
-    var P1: UInt16!
-    var P2: Int16!
-    var P3: Int16!
-    var P4: Int16!
-    var P5: Int16!
-    var P6: Int16!
-    var P7: Int16!
-    var P8: Int16!
-    var P9: Int16!
-    
-    // A value used for compensating pressure and humidity. It is calculated when compensating temperature
-    var t_fine: Int32 = 0
+    private let pointerToStartSensorCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
+    private let pointerToStartFilterCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
+    private let pointerToStartResetCommand = UnsafeMutablePointer<UInt8>.allocate(capacity: 2)
     
     
     // An enum representing each weather forecast
@@ -157,10 +139,9 @@ class AutomatCommunication {
             if error != nil{
                 print("error")
             }else{
-                print("Wrote stuff to reset")
+                print("Wrote to register")
             }
         }
-        
         let reset: NLAI2CMutableWriteData = NLAI2CMutableWriteData.init(address: 0x76)
         reset.addI2CCommands(pointerToStartResetCommand, count: 2)
         
@@ -173,11 +154,7 @@ class AutomatCommunication {
             if error != nil{
                 print("error")
             }else{
-                var a_data_u8 = responseData!.responses!
-                
-                self.T1 = UInt16((UInt16((a_data_u8[1] as! UInt8)) << 8) | UInt16(a_data_u8[0] as! UInt8))
-                self.T2 = Int16((Int16((a_data_u8[3] as! Int8)) << 8) | Int16(a_data_u8[2] as! Int8))
-                self.T3 = Int16((Int16((a_data_u8[5] as! Int8)) << 8) | Int16(a_data_u8[4] as! Int8))
+                self.compensations.setTemperatureParameters(responseData!.responses)
             }
         }
         
@@ -187,22 +164,7 @@ class AutomatCommunication {
             if error != nil{
                 print("error")
             }else{
-                var a_data_u8 = responseData!.responses!
-                
-                
-                self.P1 = UInt16((UInt16((a_data_u8[1] as! UInt8)) << 8) | UInt16(a_data_u8[0] as! UInt8))
-                print("P1 : \(self.P1)")
-                self.P2 = Int16((Int16((a_data_u8[3] as! Int8)) << 8) | Int16(a_data_u8[2] as! Int8))
-                print("P2 : \(self.P2)")
-                self.P3 = Int16((Int16((a_data_u8[5] as! Int8)) << 8) | Int16(a_data_u8[4] as! Int8))
-                print("P3 : \(self.P3)")
-                self.P4 = Int16((Int16((a_data_u8[7] as! Int8)) << 8) | Int16(a_data_u8[6] as! Int8))
-                print("P4 : \(self.P4)")
-                self.P5 = Int16((Int16((a_data_u8[9] as! Int8)) << 8) | Int16(a_data_u8[8] as! Int8))
-                self.P6 = Int16((Int16((a_data_u8[11] as! Int8)) << 8) | Int16(a_data_u8[10] as! Int8))
-                self.P7 = Int16((Int16((a_data_u8[13] as! Int8)) << 8) | Int16(a_data_u8[12] as! Int8))
-                self.P8 = Int16((Int16((a_data_u8[15] as! Int8)) << 8) | Int16(a_data_u8[14] as! Int8))
-                self.P9 = Int16((Int16((a_data_u8[17] as! Int8)) << 8) | Int16(a_data_u8[17] as! Int8))
+                self.compensations.setPressureParameters(responseData!.responses)
             }
         }
         
@@ -223,6 +185,11 @@ class AutomatCommunication {
         startMeasurementCycle()
         
     }
+    
+    
+    /**
+    * Start a new Measurement by setting a Timer object to repeat readings for 1 Minute.
+    */
     @objc func startMeasurementCycle(){
         arrayForCountingAverage = [Double]()
         timerForCountingAverage = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(newMeasurement), userInfo: nil, repeats: true)
@@ -326,10 +293,10 @@ class AutomatCommunication {
                     
                     
                     // Temperature has to be calculated, but is not needed for weather forecasting, it is therefore only printed to console
-                    print("Temperature: \(self.compensateTemperatureMeasurement(temperature))")
+                    print("Temperature: \(self.compensations.compensateTemperatureMeasurement(temperature))")
                     
                     
-                    let newPressure = self.compensatePressureMeasurement(pressure)
+                    let newPressure = self.compensations.compensatePressureMeasurement(pressure)
                     print("Pressure: \(newPressure) Pressure divided by 256: \(newPressure/256)")
                     if(self.arrayForCountingAverage != nil){
                         self.arrayForCountingAverage!.append(Double(newPressure))
@@ -353,142 +320,6 @@ class AutomatCommunication {
         else{
             print("Chip not set yet")
         }
-    }
-    
-    
-    
-    /**
-     *   Compensates the temperature value received from the BME280 sensor using calibration parameters received from the sensor.
-     *
-     *   @param uncomp : the uncompensated temperature value read directly from the sensor
-     *   @return : the compensated value of the temperature as signed Int16.
-     **/
-    private func compensateTemperatureMeasurement(_ uncomp: Int32) -> Double                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             {
-        /*  DOUBLE COMPENSATION FORMULA returns Double*/
-        
-        var x1 : Double = 0
-        var x2 : Double = 0
-        var comp : Double = 0
-        
-        x1 = ((Double(uncomp))/16384.0 - (Double(T1))/1024.0) * (Double(T2))
-        x2 = (((Double(uncomp))/131072.0 - (Double(T1))/8192.0) * ((Double(uncomp))/131072.0 - (Double(T1))/8192.0)) * (Double(T3))
-        t_fine = Int32(x1 + x2)
-        comp = (x1 + x2) / 5120.0
-        return comp
-        
-        
-        
-        
-        /* 32 - BIT COMPENSATION FORMULA returns Int32*/
-        /*
-         var comp: Int32 = 0
-         var x1: Int32 = 0
-         var x2: Int32 = 0
-         
-         // calculate x1
-         x1 = ((((uncomp >> 3) - (Int32(T1)<<1))) * (Int32(T2))) >> 11
-         
-         // calculate x2
-         x2 = (((((uncomp >> 4) - (Int32(T1))) * ((uncomp>>4) - (Int32(T1)))) >> 12) * (Int32(T3))) >> 14
-         
-         t_fine = x1 + x2
-         
-         comp = (t_fine * 5 + 128) >> 8
-         
-         return comp
-         */
-    }
-    
-    
-    private func compensatePressureMeasurement(_ uncomp: Int32) -> UInt32{
-        if t_fine != 0{
-            /* 32-BIT COMPENSATION FORMULA returns UInt32 */
-            
-            var x1 : Int32 = 0
-            var x2 : Int32 = 0
-            var comp : UInt32 = 0
-            x1 = ((Int32(t_fine))>>1) - (Int32(64000))
-            x2 = (((x1 >> 2) * (x1 >> 2)) >> 11 ) * (Int32(P6))
-            
-            x2 = x2 + ((x1 * (Int32(P5)))<<1)
-            x2 = (x2 >> 2) + ((Int32(P4))<<16)
-            x1 = (((Int32(P3)*(((x1>>2) * (x1>>2))>>13))>>3) + (((Int32(P2))*x1)>>1))>>18
-            x1 = ((((32768 + x1)) * (Int32(P1)))>>15)
-            
-            if(x1 == 0)  {
-                return 0
-            }
-            comp = ((UInt32(((Int32(1048576)) - uncomp) - (x2 >> 12)))) * 3125
-            if ( comp < 0x80000000){
-                comp = (comp<<1)/(UInt32(x1))
-            } else{
-                comp = (comp/UInt32(x1)) * 2
-            }
-            
-            x1 = ((Int32(P9)) * (Int32((((comp>>3) * (comp>>3))>>13))))>>12
-            x2 = ((Int32((comp>>2))) * (Int32(P8)))>>13
-            comp = UInt32(Int32(comp) + ((x1 + x2 + Int32(P7)) >> 4))
-            
-            return comp
-            
-            /*   DOUBLE COMPENSATION FORMULA  returns Double */
-            /*
-             var x1: Double = 0
-             var x2: Double = 0
-             var comp: Double = 0
-             
-             x1 = (Double(t_fine)/2.0) - 64000.0
-             x2 = x1 * x1 * (Double(P6)) / 32768.0
-             x2 = x2 * x1 * (Double(P5)) * 2.0
-             x2 = (x2/4.0) + ((Double(P4)) * 65536.0)
-             x1 = ((Double(P3)) * x1 * x1 / 524288.0 + (Double(P2)) * x1) / 524288.0
-             x1 = (1.0 + x1 / 32768.0) * (Double(P1))
-             if(x1 == 0.0){
-             return 0.0
-             }
-             
-             comp = 1048576.0 - Double(uncomp)
-             comp = (comp - (x2 / 4096.0)) * 6250.0 / x1
-             x1 = (Double(P9)) * comp * comp / 2147483648.0
-             x2 = comp * (Double(P8)) / 32768.0
-             comp = comp + (x1 + x2 + (Double(P7))) / 16.0
-             return comp
-             
-             */
-            
-            
-            
-            /*   64-BIT COMPENSATION FORMULA returns UInt32 */
-            /*
-             var x1: Int64 = 0
-             var x2: Int64 = 0
-             var comp: Int64 = 0
-             
-             x1 = (Int64(t_fine)) - 128000   //1
-             x2 = x1 * x1 * Int64(P6) //2
-             x2 = x2 + ((x1 * Int64(P5)) << 17)  //3
-             x2 = x2 + (Int64(P4) << 35)    //4
-             x1 = ((x1 * x1 * Int64(P3))>>8) + ((x1 * Int64(P2)) << 12) //5
-             x1 = (((Int64(1) << 47) + x1) * Int64(P1)) >> 33
-             if (x1 == 0){
-             return 0
-             } //7
-             comp = Int64(1048576) - Int64(uncomp) //8
-             comp = (((comp << 31) - x2) * 3125) / x1 //9
-             
-             x1 = (Int64(P9) * (comp >> 13) * (comp >> 13)) >> 25 //10
-             x2 = (Int64(P8) * comp) >> 19 //11
-             comp = ((comp + x1 + x2) >> 8) + ((Int64(P7)) << 4) //12
-             
-             return UInt32(comp)
-             
-             
-             */
-            
-            
-        }else{
-            print("T_fine has not been calculated. Make sure that a temperature measurement is made before calculating pressure")
-            return 0
         }
     }
-}
+
